77f305f8202d718f1f2c08a904f9efe562f43404
runchenwu <60835042+runchenwu@users.noreply.github.com>
2021-12-27 15:05:55 -0800

    auton progress

--- include/function.h
+++ include/function.h
@@ -2,7 +2,7 @@
 
 void SetVelocity(double x);
 void drivefunctions(std::string dir, double x);
-void stap (std::string option);
-void FrontLift (std::string dir, double x, bool wait);
-void BackLift (std::string dir, double x, bool wait);
+void stap(std::string option);
+void FrontLift(std::string dir, double x, bool wait);
+void BackLift(std::string dir, double x, bool wait);
 void drivesetstop();
--- src/main.cpp
+++ src/main.cpp
@@ -26,7 +26,8 @@
 using namespace vex;
 double lol = 1;
 int main() {
-  /*// Initializing Robot Configuration. DO NOT REMOVE!
+// initialization
+  // Initializing Robot Configuration. DO NOT REMOVE!
   vexcodeInit();
   
   // DRIVE FUNCTIONS : drivefunctions(direction, waittime) [fwd, rev, lft, rht]
@@ -35,7 +36,7 @@ int main() {
   // BAK LIFT FUNCTIONS : BackLift(dir, deg, wait?) [up, down]
   SetVelocity(100);
 
-//task1 pick up blue goal 1
+// task 1 pick up the blue goal #1, NOT TUNED
   // deploy front lift
   FrontLift("down", 120, false);
   wait(1, sec);
@@ -47,7 +48,7 @@ int main() {
   FrontLift("up", 70, false);
   wait(0.8, sec);
   stap("frontlift");
-//setup robot for neutral goal
+// task 2 turn left and push neutral goal #1 to other side, NOT TUNED
   // drive back out of the platform
   drivefunctions("rev", 0.45);
   stap("drivetrain");
@@ -66,40 +67,56 @@ int main() {
   wait (0.1, sec);
   drivesetstop();
   SetVelocity(100);
-
-
   wait(0.5, sec);
-//drive and push neutral goal
+  //drive and push neutral goal
   drivefunctions("fwd", 5);
   stap("drivetrain");
   drivesetstop();
-//
+// task 3 turn right and put down blue goal #1, NOT TUNED
   drivefunctions("rev", 0.5);
-//
+  //
   drivefunctions("rht", 0.4);
   stap("drivetrain");
   drivesetstop();
-drivefunctions("fwd", 0.3);
-stap("drivetrain");
-//
+  drivefunctions("fwd", 0.3);
+  stap("drivetrain");
+  //
   FrontLift("down", 70, false);
   wait(1, sec); 
   stap("frontlift");
-//
-  drivefunctions("rev", 0.3);
-  stap("drivetrain");
-//
+  
+// task 4 go forward and pick up red goal #2 and store, NOT TUNED
+  //
   BackLift("down", 95, false);
   wait(1, sec);
   stap("backlift");
-//
+  //
   drivefunctions("rev", 0.6);
   stap("drivetrain");
+  // lift up goal
+  BackLift("up", 95, false);
+  wait(1, sec);
+  stap("backlift");
+// task 5 around the neutral goal and pick up blue goal #2, NOT TUNED
+  // back up a little
+  drivefunctions("rev", 0.2);
+  stap("drivetrain");
+  // turn right 
+  drivefunctions("rht", 0.9);
+  stap("drivetrain");
+  // forward
+  drivefunctions("fwd", 3);
+  stap("drivetrain");
+  // 
+// task 6 pick up neutral goal #3, NOT TUNED
+// task 7 put down blue goal #2, NOT TUNED
 
-*/
+// task 8 pick up red goal #1, NOT TUNED
+
+// task 9 drive to the ramp, NOT TUNED
 
 
-  SetVelocity(100);
+// task 10 go up the ramp, NOT TUNED
   Inertial.resetRotation();
   drivesetstop();
   FrontLift("down", 120, false);
@@ -108,9 +125,9 @@ stap("drivetrain");
   Inertial.resetRotation();
   wait(0.5, sec);
   FrontRight.spin(reverse);
-      FrontLeft.spin(reverse);
-      BackLeft.spin(reverse);
-      BackRight.spin(reverse);
+  FrontLeft.spin(reverse);
+  BackLeft.spin(reverse);
+  BackRight.spin(reverse);
   wait(1, sec);
   FrontLift("up", 100, false);
   wait(0.2, sec);
@@ -141,12 +158,6 @@ stap("drivetrain");
       stap("drivetrain");
       drivesetstop();
     }
-  
-    
-
   }
-
-
-
 }
 
